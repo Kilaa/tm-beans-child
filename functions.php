79b6e7d6b0dbde8ec7a93ea40f4ce3a27a29b613
Michel Phuong <michelphuong@hotmail.fr>
2023-11-27 01:18:21 +0100

    Mise en place du hook
    Hook mis en place, il ne reste plus que le responsive

--- functions.php
+++ functions.php
@@ -17,3 +17,17 @@ function beans_breadcrumb_nope() {
 	return; 
 }
 beans_modify_action_callback( 'beans_main_grid_before_markup', 'beans_breadcrumb' );
+
+function modify_menu($items, $args) { 
+    if(current_user_can('administrator')){
+		
+        $position=strpos($items,"Nous rencontrer</a></li>");	
+        $longeur=strlen("Nous rencontrer</a></li>");
+        $lien_dadministration='<li><a href="' .admin_url(). '">Admin</a></li>';
+        $offset=$position+$longeur;
+        $items=substr_replace($items,$lien_dadministration,$offset,0);
+    }
+    return $items;
+}
+add_filter("wp_nav_menu_items", "modify_menu", 10,2);
+
